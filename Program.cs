f587054c73626cf82fac78a91745b5a1fab260b1
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-11-24 14:19:55 -0300

    resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenciaCentena
+        {
+            
+    class Program
+       {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1 ; i <= 100;i ++)
+            {
+            Console.Write($" {i} ");
+            
+         }
+       }
+    }
+}
+
+          
+        
+    
+
